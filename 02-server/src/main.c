20da85943451f583306dff6e996a9cc4a60ca9cb
haved <krogstie.havard@gmail.com>
2022-03-18 13:45:35 +0100

    IPv6 support

--- 02-server/src/main.c
+++ 02-server/src/main.c
@@ -38,19 +38,18 @@ int main(int argc, char *argv[]) {
 	settings.num_threads = strtol(argv[3], NULL, 10);
 	settings.buffer_slots = strtol(argv[4], NULL, 10);
 
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < -1)
+    if ((server_socket = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
         error("could not create socket\n");
 
     // Allow other processes to bind to the socket, to allow quick restarts
     int optval = 1;
     setsockopt(server_socket, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
 
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in6 server_addr = {}, client_addr = {};
+    server_addr.sin6_family = AF_INET6;
+    server_addr.sin6_addr = in6addr_any; //= IN6ADDR_ANY_INITinet_addr(ADDR);
+    server_addr.sin6_port = htons(settings.port);
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ADDR);
-    server_addr.sin_port = htons(settings.port);
-    
     if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
         error("bind error\n");
 
