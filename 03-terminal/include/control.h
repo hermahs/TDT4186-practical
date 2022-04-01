2b6f712aaad713138b5c50eeb21281c8ee4eebed
hermahs <hermahs@stud.ntnu.no>
2022-04-01 15:09:58 +0200

    avoiding merge conflicts like a boss

--- 03-terminal/include/control.h
+++ 03-terminal/include/control.h
@@ -1,3 +1,4 @@
 void handle_command(char* command, char* args);
-
 const char* get_file_from_command(char* command);
+
+extern int link[2];
--- 03-terminal/src/control.c
+++ 03-terminal/src/control.c
@@ -1,8 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "control.h"
 
-void handle_command(char* command, char* args) {
-	const char* c = get_file_from_command(command);
+#define MAX_SIZE 1024*1024
+#define err(e) do {fprintf(stderr, "%s\n", e); exit(EXIT_FAILURE); } while (0);
+
+char* handle_command(char* command, char* args) {
+	pid_t pid;
+	char output[MAX_SIZE];
+
+	if (pipe(link) == -1) err("pipe");
+
+	if ((pid = fork()) == -1) err("fork");
+
+	if (pid == 0) {
+		dup2(link[1], STDOUT_FILENO);
+		close(link[0]);
+		close(link[1]);
+		execl("/bin/ls", "ls", "-1", (char*) NULL);
+		err("execl");
+	} else {
+		close(link[1]);
+		int nbytes = read(link[0], output, sizeof(output));
+		printf("Output: %s\n", output);
+		return output;
+	}
+
 	
+		
 }
 
 const char* get_file_from_command(char* command) {
